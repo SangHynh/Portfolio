5bf863ccd7b7efc27221378cb3396d7cbea3b1ac
SangHynh <huynhvansang.020102@gmail.com>
2024-07-01 01:22:33 +0700

    create hero image

---
+++ src/components/HeroImg/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./HeroImgStyles.css";
+import IntroImg from "../../assets/img/bocchi.jpg";
+import { Link } from "react-router-dom";
+
+const HeroImg = () => {
+  return (
+    <div className="hero__container">
+      <div className="hero__mask">
+        <img className="intro-img" src={IntroImg} alt="IntroImage" />
+      </div>
+      <div className="hero__content">
+        <p>
+          HELLO, I'M HUYNH VAN SANG
+          <h1>Front-end Developer</h1>
+          <Link to="/project" className="button primary-button">Projects</Link>
+          <Link to="/contact" className="button secondary-button">Contact</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImg;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HeroImg from '../components/HeroImg'
 
 const Home = () => {
   return (
     <div>
-      <Navbar></Navbar>
+      <Navbar/>
+      <HeroImg/>
     </div>
   )
 }
