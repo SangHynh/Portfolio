ab93028b40717aa87788139881cb0703d4cbe48d
SangHynh <huynhvansang.020102@gmail.com>
2024-07-05 00:06:07 +0700

    add send mail feature

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,22 +1,56 @@
-import "./FormStyles.css"
-import React from 'react'
+import "./FormStyles.css";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const Form = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_SERVICE_ID,
+        process.env.REACT_APP_TEMPLATE_ID,
+        form.current,
+        {
+          publicKey: process.env.REACT_APP_PUBLIC_KEY,
+        }
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+          console.log(process.env.REACT_APP_SERVICE_ID);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
     <div className="form__container">
-      <form action="">
-        <label htmlFor="">Your name:</label>
-        <input type="text" />
-        <label htmlFor="">Email:</label>
-        <input type="email" />
-        <label htmlFor="">Subject:</label>
-        <input type="text" />
-        <label htmlFor="">Message:</label>
-        <textarea type="text" rows={6} placeholder="Type your message here"/>
-        <button className="button form-button">Submit</button>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Your name:</label>
+        <input type="text" name="user_name" />
+        <label>Email:</label>
+        <input type="email" name="user_email" />
+        <label>Subject:</label>
+        <input type="text" name="subject" />
+        <label>Message:</label>
+        <textarea
+          name="message"
+          type="text"
+          rows={6}
+          placeholder="Type your message here"
+        />
+        <button className="button form-button" type="submit">
+          Submit
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
